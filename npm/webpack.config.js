2da865b6cc10bb80df545f6f267374e1b3cc387d
gerry.zhong <qiang.zhong02@ele.me>
2019-08-09 21:08:09 +0800

    npm publish

--- npm/webpack.config.js
+++ npm/webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: "commonjs"
   },
   module: {
     rules: [
